99aeafd6030d2f317951fc15eaa6a6e12691b1ab
Tom Klinovsky <tom.klino@gmail.com>
2020-05-09 18:30:52 +0300

    farmers list template

---
+++ src/components/FarmersList.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex v-for="farmer in farmers" xs4 :key="farmer.id">
+        <v-card
+          class="mx-auto"
+          max-width="400"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            :src="require('../assets/strawberries.jpg')"
+          >
+            <v-card-title>{{ farmer.name }}</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">{{ farmer.produce }}</v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>Order Minimum: {{ farmer.orderMinimum }}</div>
+
+            <div>Area: {{ farmer.shipmentArea }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              color="orange"
+              text
+            >
+              Order
+            </v-btn>
+
+            <v-btn
+              color="orange"
+              text
+            >
+              Share
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'FarmersList',
+  mounted () {
+    this.searchFarmers();
+  },
+  data () {
+    return {
+      farmers: []
+    }
+  },
+  methods: {
+    searchFarmers() {
+      this.farmers = [
+        {
+          name: "Tzachi",
+          produce: "Strawberries",
+          orderMinimum: 10,
+          shipmentArea: "Haruzim",
+        },
+        {
+          name: "Dudi",
+          produce: "Mangos",
+          orderMinimum: 50,
+          shipmentArea: "Haruzim",
+        },
+        {
+          name: "Yonni",
+          produce: "Kiwis",
+          orderMinimum: 20,
+          shipmentArea: "Haruzim",
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/views/FarmersListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <FarmersList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FarmersList from '@/components/FarmersList.vue'
+
+export default {
+  name: 'FarmersListView',
+  components: {
+    FarmersList
+  }
+}
+</script>
