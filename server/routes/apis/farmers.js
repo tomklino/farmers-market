405396fa30182c14fcfbca028ee541697ceadd3a
Tom Klinovsky <tom.klino@gmail.com>
2020-05-12 23:54:15 +0300

    responding with a sample list of farmers to test in the frontend

--- server/routes/apis/farmers.js
+++ server/routes/apis/farmers.js
@@ -1,8 +1,11 @@
+var fs = require('fs');
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('respond with farmers');
+  payload = fs.readFileSync("samples/farmers.json");
+  res.send(payload);
 });
 
 module.exports = router;
