2a2ada101d522a3f8fa34a5bcb23e4efbb0bbed2
Tom Klinovsky <tom.klino@gmail.com>
2020-05-29 15:00:53 +0300

    WIP: creating an order page

--- frontend/src/components/FarmerOrderForm.vue
+++ frontend/src/components/FarmerOrderForm.vue
@@ -1,18 +1,51 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <v-card>
-      <v-card-title>{{produce}}</v-card-title>
-      <v-form v-model="valid">
-        <v-layout row wrap>
-          <v-row>
-            <v-text-field
-              v-model="quantity"
-              type="number"
-              label="How many"
-              ></v-text-field>
-          </v-row>
-        </v-layout>
-      </v-form>
+    <v-card
+      class="mx-auto"
+      max-width="480"
+      >
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="farmer.image"
+      >
+        <v-card-title>{{ farmer.name }}</v-card-title>
+      </v-img>
+      <v-card-title>{{ farmer.produce }}</v-card-title>
+      <v-card-text class="text-left">
+        <p class="text--primary">
+          Selling in units of {{ farmer.packageSize }} {{farmer.packageUnit }}
+        </p>
+        <p class="text--primary">
+          Unit price: {{ farmer.price }}₪
+        </p>
+      </v-card-text>
+      <v-card-text>
+        <v-form class="px-3" v-model="valid">
+          <v-text-field
+            v-model="quantity"
+            type="number"
+            label="How many"
+            ></v-text-field>
+          <v-text-field
+            v-model="name"
+            type="text"
+            label="Your full name"
+            :rules="nameRules"
+            ></v-text-field>
+          <v-text-field
+            v-model="phone"
+            type="text"
+            label="Phone number"
+            :rules="phoneRules"
+            ></v-text-field>
+          <v-btn
+            large
+            color="success"
+            v-bind:disabled="!valid"
+            >Complete Order</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
@@ -28,12 +61,21 @@ export default {
     console.log(store.state.displayedFarmer);
   },
   data: () => ({
+    name: "",
+    nameRules: [
+      v => !!v || 'Name is required'
+    ],
+    phoneRules: [
+      v => !!v || 'Phone is required'
+      // TODO match phone regex
+    ],
+    phone: "",
     valid: false,
     quantity: 1,
   }),
   computed: {
-    produce() {
-      return store.state.displayedFarmer.produce;
+    farmer() {
+      return store.state.displayedFarmer
     }
   }
 }
