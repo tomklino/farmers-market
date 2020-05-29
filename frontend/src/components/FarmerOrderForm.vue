3ecfa4d89a4bb8b4414a5114b4527bda926bb4a7
Tom Klinovsky <tom.klino@gmail.com>
2020-05-30 01:10:30 +0300

    added the disabled on processing behaviour and back to main page after success

--- frontend/src/components/FarmerOrderForm.vue
+++ frontend/src/components/FarmerOrderForm.vue
@@ -1,54 +1,74 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-card
-      class="mx-auto"
-      max-width="480"
-      >
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        :src="farmer.image"
-      >
-        <v-card-title>{{ farmer.name }}</v-card-title>
-      </v-img>
-      <v-card-title>{{ farmer.produce }}</v-card-title>
-      <v-card-text class="text-left">
-        <p class="text--primary">
-          Selling in units of {{ farmer.packageSize }} {{farmer.packageUnit }}
-        </p>
-        <p class="text--primary">
-          Unit price: {{ farmer.price }}₪
-        </p>
-      </v-card-text>
-      <v-card-text>
-        <v-form class="px-3" v-model="valid">
-          <v-text-field
-            v-model="quantity"
-            type="number"
-            label="How many"
-            ></v-text-field>
-          <v-text-field
-            v-model="name"
-            type="text"
-            label="Your full name"
-            :rules="nameRules"
-            ></v-text-field>
-          <v-text-field
-            v-model="phone"
-            type="text"
-            label="Phone number"
-            :rules="phoneRules"
-            ></v-text-field>
+  <div>
+    <v-dialog
+      persistent
+      v-model="completedDialogOpened"
+      width="500"
+    >
+      <v-card>
+        <v-card-title>Success!</v-card-title>
+        <v-card-actions>
           <v-btn
-            large
-            color="success"
-            v-bind:disabled="!valid"
-            @click="commitOrder"
-            >Complete Order</v-btn>
-        </v-form>
-      </v-card-text>
-    </v-card>
-  </v-container>
+            text
+            @click="routeToFarmers"
+            >Back To Farmers Page</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-container grid-list-md text-xs-center>
+      <v-card
+        class="mx-auto"
+        max-width="480"
+        >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="farmer.image"
+        >
+          <v-card-title>{{ farmer.name }}</v-card-title>
+        </v-img>
+        <v-card-title>{{ farmer.produce }}</v-card-title>
+        <v-card-text class="text-left">
+          <p class="text--primary">
+            Selling in units of {{ farmer.packageSize }} {{farmer.packageUnit }}
+          </p>
+          <p class="text--primary">
+            Unit price: {{ farmer.price }}₪
+          </p>
+        </v-card-text>
+        <v-card-text>
+          <v-form class="px-3" v-model="valid">
+            <v-text-field
+              v-model="quantity"
+              type="number"
+              label="How many"
+              :disabled="isDisabled"
+              ></v-text-field>
+            <v-text-field
+              v-model="name"
+              type="text"
+              label="Your full name"
+              :rules="nameRules"
+              :disabled="isDisabled"
+              ></v-text-field>
+            <v-text-field
+              v-model="phone"
+              type="text"
+              label="Phone number"
+              :rules="phoneRules"
+              :disabled="isDisabled"
+              ></v-text-field>
+            <v-btn
+              large
+              color="success"
+              v-bind:disabled="!valid || isDisabled"
+              @click="commitOrder"
+              >Complete Order</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -63,6 +83,9 @@ export default {
     console.log(store.state.displayedFarmer);
   },
   methods: {
+    routeToFarmers() {
+      this.$router.push("/farmers");
+    },
     async commitOrder() {
       let payload = {
         name: this.name,
@@ -73,11 +96,15 @@ export default {
       }
 
       console.log(payload);
+      this.isDisabled = true;
       let newOrderResponse = await axios.post('/api/orders/new', payload);
       console.log("order submitted:", newOrderResponse);
+      this.completedDialogOpened = true;
     }
   },
   data: () => ({
+    completedDialogOpened: false,
+    isDisabled: false,
     name: "",
     nameRules: [
       v => !!v || 'Name is required'
