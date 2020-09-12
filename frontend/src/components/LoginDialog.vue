9d853206f636eb7d263670d13cb2e55fd74c2dc1
Tom Klinovsky <tom.klino@gmail.com>
2020-09-12 23:57:17 +0300

    WIP: installed sign in with google - button on the frontend - no logic on the backend yet

--- frontend/src/components/LoginDialog.vue
+++ frontend/src/components/LoginDialog.vue
@@ -11,6 +11,13 @@
           type="password"
           label="password"></v-text-field>
       </v-card-text>
+      <v-card-text>
+        <GoogleLogin
+          :params="googleParams"
+          :onSuccess="googleOnSuccess"
+          :onFailure="googleOnFailure"
+        >Login</GoogleLogin>
+      </v-card-text>
       <v-card-actions>
         <v-btn large color="green"
           v-if="!isLoggedIn()"
@@ -29,12 +36,16 @@
 
 <script>
 import axios from 'axios';
+import GoogleLogin from 'vue-google-login';
 import store from '@/store';
 
 export default {
   name: "LoginDialog",
   props: {
     value: Boolean
+  },
+  components: {
+    GoogleLogin
   },
   computed: {
     loginString() {
@@ -54,9 +65,21 @@ export default {
     }
   },
   data: () => ({
-    devadminPassword: ''
+    devadminPassword: '',
+    googleParams: {
+      client_id: "573809548678-m51b16050trf1hpd5o6nlv4u5irjbntt.apps.googleusercontent.com"
+    }
   }),
   methods: {
+    googleOnSuccess(googleUser) {
+      let id_token = googleUser.getAuthResponse().id_token;
+
+      console.log("success, google user is", googleUser);
+      console.log("the id_token is", id_token);
+    },
+    googleOnFailure(obj) {
+      console.log("failure to login", obj);
+    },
     isLoggedIn() {
       return store.state.loggedInUser.loggedIn;
     },
