37c86a474a7ee06145a6bc6b6202996203922a85
Tom Klinovsky <tom.klino@gmail.com>
2020-10-23 15:28:26 +0300

    some tests

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -3,6 +3,13 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.contains('החקלאים')
+  })
+})
+
+describe('Making an order test', () => {
+  it('Visits an order form', () => {
+    cy.visit('/')
+    cy.contains('a', 'תום המשוגע').click({ force: true });
   })
 })
