7c9636ef39977b38c200f64d3b461a129547a27d
Tom Klinovsky <tom.klino@gmail.com>
2020-11-27 18:25:39 +0200

    WIP: making a permissions middleware

--- server/rbac/index.js
+++ server/rbac/index.js
@@ -1,65 +1,47 @@
 const AccessControl = require('role-acl');
+const permissionsData = require('../data-modules/permissions-data');
 
-const grants = {
-  admin: {
-    grants: [
-      {
-        resource: 'farmer',
-        action: '*',
-        attributes: ['*']
-      },
-      {
-        resource: 'order',
-        action: '*',
-        attributes: ['*']
-      }
-    ]
-  },
-  user: {
-    grants: [
-      {
-        resource: 'order',
-        action: ['read', 'modify', 'list'],
-        attributes: ['*'],
-        condition: {
-          Fn: 'EQUALS',
-          args: {
-            'requester': '$.created_by'
-          }
-        }
-      },
-      {
-        resource: 'order',
-        action: 'create'
-      }
-    ]
-  },
-  farmer: {
-    grants: [
-      {
-        resource: 'farmer',
-        action: ['modify'],
-        attributes: ['*', '!owner'],
-        condition: {
-          Fn: 'EQUALS',
-          args: {
-            'requester': '$.owner'
-          }
-        }
-      },
-      {
-        resource: 'order',
-        action: ['read', 'list', 'complete'],
-        attributes: ['products', 'completed', 'payed', 'name'],
-        condition: {
-          Fn: 'EQUALS',
-          args: {
-            'requester': '$.farmer_id_on_order'
-          }
-        }
-      }
-    ]
+module.exports = rbac;
+
+function rbac(options) {
+  return async (req, res, next) => {
+    let requiredVerb = "";
+    if(typeof options.verb === string) {
+      requiredVerb = options.verb;
+    } else {
+      requiredVerb = getVerbFromMethod(req.method)
+    }
+
+    // TODO make those two flexible by allowing to pass a function in options that will retrieve them in another way
+    let resource = req.params['id']
+    let role = req.session['username']
+
+    let rolePermissions = permissionsData.getPermissionsForRole(role);
+
+    let ac = new AccessControl(rolePermissions);
+    let permission = await ac.can(role).execute(requiredVerb).on(resource);
+
+    if(permission.granted) {
+      req.permission = permission;
+      return next();
+    } else {
+      res.status(403).json({ message: "Not allowed" });
+    }
   }
 }
 
-module.exports = new AccessControl(grants);
+function getVerbFromMethod(method) {
+  switch (method) {
+    case "GET":
+      return "read";
+      break;
+    case "POST":
+      return "create";
+      break;
+    case "PUT":
+      return "modify";
+      break;
+    default:
+      return "*";
+  }
+}
